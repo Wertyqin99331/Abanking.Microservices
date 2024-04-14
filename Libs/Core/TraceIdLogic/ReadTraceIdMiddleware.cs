8e345ba3da482684a45e50150468c34e0bf3b068
Wertyqin99331 <flywotbl@mail.ru>
2024-04-14 18:08:01 +0500

    http logic dz final

---
+++ Libs/Core/TraceIdLogic/ReadTraceIdMiddleware.cs
@@ -0,0 +1,15 @@
+﻿using Core.TraceLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.TraceIdLogic;
+
+public class ReadTraceIdMiddleware(IEnumerable<ITraceReader> traceReaders): IMiddleware
+{
+	public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+	{
+		foreach (var traceReader in traceReaders)
+			traceReader.WriteValue(context.Request.Headers["TraceId"]);
+
+		await next(context);
+	}
+}
---
+++ Libs/Core/TraceIdLogic/WriteTraceIdMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Serialization;
+using ITraceWriter = Core.TraceLogic.Interfaces.ITraceWriter;
+
+namespace Core.TraceIdLogic;
+
+public sealed class WriteTraceIdMiddleware(IEnumerable<ITraceWriter> traceWriters): IMiddleware
+{
+	public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+	{
+		foreach (var traceWriter in traceWriters)
+			traceWriter.GetValue();
+
+		await next(context);
+	}
+}
